fde1a1006441fc1100bde804d0361ce686fe70fd
willju-wangqian <wju@iastate.edu>
2021-11-11 19:36:15 -0600

    added results for q3

--- homework3/q3_code.R
+++ homework3/q3_code.R
@@ -136,5 +136,101 @@ selected.region <- list(r1, r2, r3)
 save(selected.region, file="local/selected_region.rda")
 
 
+ests <- list()
+for(i in 1:18) {
+  ests[[i]] <- list()
+  for (j in 1:3) {
+    ests[[i]][[j]] <- readRDS(
+      paste("homework3/results_q3/result-", j, "-", i, "-est.rds", sep = "")
+      )
+  }
+}
+
+variance.est.each <- function(par, r, sample.size = 1000, FUNC = logf){
+  sigma1 <- par[1]
+  sigma2 <- par[2]
+  rho <- par[3]
+  tau <- par[4]
+
+  theta.cond <- arms(sample.size, FUNC, -pi, pi, metropolis = TRUE,
+                     arguments = list(
+                       r=r, sigma1=sigma1, sigma2=sigma2, rho=rho, tau=tau
+                     ))
+
+  part.sigma1 <- mean(-2*cos(theta.cond)^2/sigma1^3 + (2*rho*sin(theta.cond)*cos(theta.cond))/(sigma1^2*sigma2))
+  part.sigma2 <- mean(-2*sin(theta.cond)^2/sigma2^3 + (2*rho*sin(theta.cond)*cos(theta.cond))/(sigma1*sigma2^2))
+  part.rho1 <- mean(cos(theta.cond)^2/sigma1^2 - (2*rho*cos(theta.cond)*sin(theta.cond))/(sigma1*sigma2) +
+    sin(theta.cond)^2/sigma2^2)
+  part.rho2 <- mean(-2*sin(theta.cond)*cos(theta.cond)/(sigma1*sigma2))
+  part.tau <- part.rho1
+
+  dev.sigma1 <- -1/sigma1 - 1/(2*(1-rho^2))*(r-tau)^2*part.sigma1
+  dev.sigma2 <- -1/sigma2 - 1/(2*(1-rho^2))*(r-tau)^2*part.sigma2
+  dev.rho <- rho/(1-rho^2) + (r-tau)^2 * (-rho/(1-rho^2)^2 * part.rho1 - 1/(2*(1-rho^2)) * part.rho2)
+  dev.tau <- (r-tau)/(1-rho^2) * part.tau
+
+  q <- c(dev.sigma1, dev.sigma2, dev.rho, dev.tau)
+
+  I.matrix <- q %*% t(q)
+
+  return(I.matrix)
+}
+
+load("~/STAT_680/STAT680/local/selected_region.rda")
+
+I.est <- variance.est.each(ests[[1]][[2]], selected.region[[2]][,,1][1])
+
+
+variance.est <- function(par, r.vec, sample.size = 1000, return_inv = TRUE){
+  result <- lapply(r.vec, function(r) {
+    variance.est.each(par, r, sample.size)
+  })
+
+  result <- Reduce("+", result)
+
+  if(return_inv) {
+    return(solve(result))
+  } else {
+    return(result)
+  }
+
+}
+
+ests.var <- list()
+for(i in 1:18) {
+  ests.var[[i]] <- list()
+  for(j in 1:3) {
+    ests.var[[i]][[j]] <- variance.est(par=ests[[i]][[j]], r.vec=c(selected.region[[j]][,,i]) )
+    cat("finish for i:", i, "j:", j, "\n")
+  }
+}
+
+library(tidyverse)
+result.container <- expand.grid(region=1:3, image=1:18) %>% as_tibble()
+
+result.container <- result.container %>% mutate(
+  estimates = purrr::map2(region, image, function(j, i) {
+    ests[[i]][[j]]
+  }),
+  variance.matrix = purrr::map2(region, image, function(j, i) {
+    ests.var[[i]][[j]]
+  }),
+  variance = purrr::map(variance.matrix, diag)
+)
+
+result.container <- result.container %>% unnest(c(estimates, variance))
+result.container$parameter <- rep(c("sigma1", "sigma2", "rho", "tau"), 18 * 3)
+
+result.container <- result.container %>% mutate(
+  lower = estimates - qnorm(.975) * sqrt(variance),
+  upper = estimates + qnorm(.975) * sqrt(variance)
+)
+
+save(result.container, ests, ests.var, file="homework3/q3_all_results.rda")
+
+
+
+
+
 
 
