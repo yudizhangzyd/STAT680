a7c3736b65b0b54a7fb33fbb1bfef1bee91cd763
willju-wangqian <wju@iastate.edu>
2021-11-28 20:34:11 -0600

    update the parallel version

--- homework4/q2c_script_parallel.R
+++ homework4/q2c_script_parallel.R
@@ -21,17 +21,19 @@ x_prob_parallel <- function(i, j, z, par, n.core) {
 }
 
 
-n.core <- 13
+n.core <- 12
 
 cl <- parallel::makeCluster(n.core)
 doParallel::registerDoParallel(cl)
 
 ### (c)
 dim.z <- c(512, 512)
+# dim.z <- c(16, 16)
 par <- c(0, 1, 1)
+# par <- c(0.25, 0.5, 0.5)
 
-result.c <- find.perfect.sample(dim.z, par, seed = 18271128)
-saveRDS(result.c, file = "h4_q2c_result/c_perfect_sample_512.rds")
+result.c <- find.perfect.sample(dim.z, par, n.core=n.core, seed = 17381128, m.start = 25)
+saveRDS(result.c, file = "h4_q2c_result/c_perfect_sample_512_parallel.rds")
 
 parallel::stopCluster(cl)
 
